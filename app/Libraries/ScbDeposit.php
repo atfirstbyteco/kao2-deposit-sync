9c1ddf250aaf90b6effee3d0d4a15018dbfa7ca8
Jarak Kritkiattisak <admin@mycools.in.th>
2018-08-29 15:36:33 +0700

    ,,,

--- app/Libraries/ScbDeposit.php
+++ app/Libraries/ScbDeposit.php
@@ -40,22 +40,26 @@ class ScbDeposit {
         $headers = array_merge($this->headers,[
             'accountNumber' => $accountNo
         ]);
+        $requestbody = [
+            "accountNumber" => $accountNo,
+            'accountCurrency' => env('SCB_DEPOSIT_HEADER_ACCOUNT_CURRENCY',764),
+            'includeBalance' => env('SCB_DEPOSIT_HEADER_INCLUDE_BALANCE',true),
+            'includeExtBalance' => env('SCB_DEPOSIT_HEADER_INCLUDE_EXT_BALANCE',true),
+            'includeInterest' => env('SCB_DEPOSIT_HEADER_INCLUDE_INTEREST',true)
+        ];
         try{
 
             $request = $this->client->post('accounts/deposits/inquiry', [
                 'headers' => $this->headers,
                 'verify' => false,
-                'json' => [
-                        "accountNumber" => $accountNo,
-                        'accountCurrency' => env('SCB_DEPOSIT_HEADER_ACCOUNT_CURRENCY',764),
-                        'includeBalance' => env('SCB_DEPOSIT_HEADER_INCLUDE_BALANCE',true),
-                        'includeExtBalance' => env('SCB_DEPOSIT_HEADER_INCLUDE_EXT_BALANCE',true),
-                        'includeInterest' => env('SCB_DEPOSIT_HEADER_INCLUDE_INTEREST',true)
-                ]
+                'body' => json_encode($requestbody)
             ]);
             return json_decode($request->getBody()->getContents());
         }catch(\Exception $e){
-            \Log::critical($e->getMessage(),$headers);
+            \Log::critical($e->getMessage(),[
+                'header' => json_encode($this->headers),
+                'requestbody' => json_encode($requestbody)
+            ]);
             return false;
         }
 
