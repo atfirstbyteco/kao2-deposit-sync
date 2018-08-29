16b6648ad9a5bb29802f6eee1e4bc24d2c0ea3f0
Jarak Kritkiattisak <admin@mycools.in.th>
2018-08-29 10:30:44 +0700

    ...

--- deposit.js
+++ deposit.js
@@ -16,6 +16,7 @@ if(process.env.HTTPS==true){
     };
     var server = require('https');
     server.createServer(options, app).listen(process.env.PORT || 8443);
+    console.log('Secure under HTTPS');
 }else{
     var server = require('http');
     server.createServer(app).listen(process.env.PORT || 8443);
