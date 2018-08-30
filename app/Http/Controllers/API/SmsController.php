842f8b833c733c9c8c3aa06adf0c9d3fb5a7ce0f
Jarak Kritkiattisak <admin@mycools.in.th>
2018-08-30 17:56:40 +0700

    ref

--- app/Http/Controllers/API/SmsController.php
+++ app/Http/Controllers/API/SmsController.php
@@ -17,7 +17,7 @@ class SmsController extends Controller
             'account_no' => $accountNo,
             'active' => 1
         ])->firstOrFail();
-        \Log::debug("BODY",$request->all());
+        \Log::debug("BODY : ",$request->all());
         $amount = ($request->get('amount'))?$request->get('amount'):10;
         $mobileno = ($request->get('mobileno'))?$request->get('mobileno'):'66XXXXXXXXX';
         $ref = $request->get('ref');
